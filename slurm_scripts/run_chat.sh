49dbf198861ab7e0da4e0a156b4cf7bfd61eb634
ShauryaKishore Panwar <spanwar@eu-login-13.euler.ethz.ch>
2026-05-27 15:34:26 +0200

    [test]chat: testing the chat script on custom data

--- slurm_scripts/run_chat.sh
+++ slurm_scripts/run_chat.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
+
 #SBATCH --gpus=nvidia_a100_80gb_pcie:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=6G
 #SBATCH --time=1:00:00
-#SBATCH --output=/cluster/work/igp_psr/spanwar/G2VLM/logs/%j.out
+#SBATCH --output=/cluster/work/igp_psr/spanwar/G2VLM/logs/run_chat_%j.out
 
-REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+REPO_DIR="/cluster/work/igp_psr/spanwar/G2VLM"
+mv "$REPO_DIR/logs/run_chat_${SLURM_JOB_ID}.out" "$REPO_DIR/logs/run_chat_$(date +%d%m-%H%M%S)_${SLURM_JOB_ID}.out"
 
 source /cluster/work/igp_psr/spanwar/envs/g2vlm_env/bin/activate
 
 nvidia-smi
 
 START=$(date +%s)
+# python -u "$REPO_DIR/inference_chat.py" \
+#     --model-path "$REPO_DIR/models/G2VLM-2B-MoT" \
+#     --image-path "$REPO_DIR/examples/25_0.jpg" \
+#     --question "Your question here"
+
 python -u "$REPO_DIR/inference_chat.py" \
     --model-path "$REPO_DIR/models/G2VLM-2B-MoT" \
-    --image-path "$REPO_DIR/examples/25_0.jpg" \
-    --question "Your question here"
+    --image-path "$REPO_DIR/examples/prs_view1.jpg" "$REPO_DIR/examples/prs_view2.jpg" \
+    --question "What movement would produce the second image from the perspective of the first?"
+
 END=$(date +%s)
 echo "Chat inference took $((END - START)) seconds"
+
